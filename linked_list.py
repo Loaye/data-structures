f29894e950bec2b9a7393cc083671378bc7318be
Loaye <jr.carstens00@gmail.com>
2017-10-25 16:02:13 -0700

    changes naming conventions of files to make more readable

--- linked_list.py
+++ linked_list.py
@@ -1,46 +1,79 @@
-"""."""
+"""Singly linked list creation."""
 
 
 class Node(object):
-    """."""
+    """Creates a node object"""
 
-    def __init__(self, val, next=None):
-         """Create a new instantiation of a node."""
-        self.val = val
+    def __init__(self, data, next):
+        """Constructor for the Node object."""
+        self.data = data
         self.next = next
 
 
-    def get_data(self):
-        return self.val
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, new_next):
-        self.next = new_next
-
-
 class LinkedList(object):
-    """."""
+    """Class for containing object called LinkedList"""
 
-    def __init__(self, head=None):
-        """."""
-        self.head = head
-
+    def __init__(self, iterable=()):
+        """Constructor for the Linked List object."""
+        self.head = None
+        self._counter = 0
+        if isinstance(iterable, (str, tuple, list)):
+            for item in iterable:
+                self.push(item)
 
     def push(self, val):
-        """."""
-        new Node(val)
-        node.next = head
+        """Add a new value to the head of the linked list."""
+        new_head = Node(val, self.head)
+        self.head = new_head
+        self._counter += 1
+
+    def pop(self):
+        """Remove & return the value of the head of the Linked List."""
+        if not self.head:
+            raise IndexError("The list is empty, so there's nothing to pop.")
+        output = self.head.data
+        self.head = self.head.next
+        self._counter -= 1
+        return output
+
+    def size(self):
+        """Return the size of our list."""
+        return self._counter
+
+    def __len__(self):
+        """Works with len() function to find length of linked list"""
+        return self._counter
+
+    def search(self, val):
+        """Search for a given node value and returns it"""
+        curr = self.head
+        while curr.next:
+            while curr.data == val:
+                return curr
+            curr = curr.next
+
+    
+    def remove(self, value):
+        """Search for a given node value and remove it."""
+        curr = self.head
+        while curr.next:
+            while curr.next.data == value:
+                curr.next = curr.next.next
+            curr = curr.next
 
 
-    def remove(self, val)
+    def display(self):
+        """Return a unicode string representing the list as if it were a Python tuple literal: “(12, ‘sam’, 37, ‘tango’)”"""
+        st = "("
+        curr = self.head
+        while curr.next is not None:
+            st += "'{}', ".format(curr.data)
+            curr = curr.next
+        st = st[:-2] + ")"
+        return st
 
 
-def search(self, val)
-
-def display()
-
-def pop()
-
-# next=None
+a_list = [4, 3, 2, 6, 4, 9, 8]
+newList = LinkedList(a_list)
+newList.push(1)
+print(newList.display())
