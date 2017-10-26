74a9bf76401c94d4a35c29aef62fb5b2e3f9de8f
Michael Shinners <mishinners@icloud.com>
2017-10-25 19:20:48 -0700

    scaffold for defs in dll. No tests written yet.

--- src/dll.py
+++ src/dll.py
@@ -2,20 +2,20 @@
 
 
 class Node(object):
-    """."""
+    """This creates a node object."""
 
     def __init__(self, data, next, prev):
-        """."""
+        """Constructor for the Node object."""
         self.data = data
         self.next = next
         self.prev = prev
 
 
 class DoublyLinkedList(object):
-    """."""
+    """The class for containing an object called DoublyLinkedList."""
 
     def __init__(self, iterable=[]):
-        """."""
+        """Constructor for the Doubly Linked List object."""
         self.head = None
         self.tail = None
         self._counter = 0
@@ -24,7 +24,7 @@ class DoublyLinkedList(object):
                 self.push(item)
 
     def push(self, val):
-        """."""
+        """Add a new value to the head of the linked list."""
         # curr = Node(val, self.head)
         # self.head = curr.next.prev
         # self._counter += 1
@@ -33,13 +33,30 @@ class DoublyLinkedList(object):
         """."""
 
     def pop(self):
-        """."""
+        """Remove & return the value of the head of the DLL."""
+        # if not self.head:
+        #     raise IndexError("The List is empty, so there's nothing to pop.")
+        # output = self.head.data
+        # self.head = self.head.next
+        # self._counter -= 1
+        # return output
 
     def shift(self):
         """."""
 
     def remove(self, val):
-        """."""
+        """Search for a given node value and remove it."""
+        # curr = self.head
+        # while curr:
+        #     if curr.next == node:
+        #         curr.next = curr.next.next
+        #     curr = curr.next
+        # return curr
 
     def size(self):
-        """."""
+        """Return the size of our list."""
+        # return self._counter
+
+    def __len__(self):
+        """Will work with the len() function to find length of the DLL."""
+        # return self.counter
