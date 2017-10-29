3349c9494fc6dbb838432690ba9b98b4eb2d817c
Loaye <jr.carstens00@gmail.com>
2017-10-29 09:51:42 -0700

    adds push and __init__ to the stack

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,22 @@
-"""Creates a stack - a subset of Linked List."""
-from linked_list_code_base import LinkedList
+"""File implements a stack with push and pop methods."""
+from linked_list import LinkedList
 
 
-class Stack(LinkedList):
-    """Creates a Stack."""
-    
+class Stack(object):
+    """Create Stack class object."""
+
+    def __init__(self, iterable=()):
+        """Init method for Stack.
+        Stack is composed of LinkedList methods and attributes.
+        """
+        self._linkedlist = LinkedList(iterable)
+        self.head = self._linkedlist.head
+        self._length = self._linkedlist._length
+
+    def push(self, data):
+        """Push method for Stack. Adds node at head."""
+        self._linkedlist.push(data)
+
     def pop(self):
-        """Remove the top node and returns it's value"""
-        try:
-            return self._values.pop()
-        except IndexError:
-            raise IndexError('Can not pop from empty stack')
+        """Pop method for Stack. Removes node at head."""
+        self._linkedlist.pop()
--- src/test_stack.py
+++ src/test_stack.py
@@ -1,15 +1,11 @@
 """Test for the stack."""
 
-import pytest
-
-
 def test_stack_push_adds_new_item():
     """Pushing creates a new head."""
     from stack import Stack
     s = Stack()
     s.push('val')
     assert s.head.data == 'val'
-
 
 def test_stack_pop_removes_head_and_returns_value():
     """Pop removes head."""
