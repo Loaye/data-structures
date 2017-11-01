38c6cc196b30c9d1aacb181fc1181d23747f4314
Michael Shinners <mishinners@icloud.com>
2017-11-01 13:36:35 -0700

    working on tests, but things are broken. Switching assignments.

--- src/heap.py
+++ src/heap.py
@@ -1,25 +1,27 @@
 """Create a Max Heap."""
 
 
-class Node(object):
-    """Create a node object."""
+# class Node(object):
+#     """Create a node object."""
 
-    def __init__(self, data, node_left, node_right, parent):
-        """Initialize a new node."""
-        self.data = data
-        self.node_left = node_left
-        self.node_right = node_right
-        self.parent = parent
+#     def __init__(self, data, node_left, node_right, parent):
+#         """Initialize a new node."""
+#         self.data = data
+#         self.node_left = node_left
+#         self.node_right = node_right
+#         self.parent = parent
 
 
 class BinaryHeap(object):
     """Instantiates a heap organized by max data values."""
 
-    def __init__(self, items=[]):
+    def __init__(self):
         """."""
-        for i in items:
-            self.heap.append(i)
-            self.__floatUp(len(self.heap) - 1)
+        # for i in items:
+        self.heap = []
+        self.size = 0
+        # self.heap.append(i)
+        # self.__floatUp(len(self.heap) - 1)
 
     def push(self, data):
         """Push a node into a heap using breadth first."""
