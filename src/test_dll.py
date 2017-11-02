e9a34ca5d3ab4f22d64133072ad83fda98c24806
Michael Shinners <mishinners@icloud.com>
2017-11-02 14:07:29 -0700

    fixed test_dll.py merge conflicts

--- src/test_dll.py
+++ src/test_dll.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-"""Test for DLL."""
-
+"""test for Doubly linked list."""
 
 import pytest
 
@@ -21,22 +19,14 @@ def dll_three_nodes():
     d.push(10)
     d.push(15)
     return d
-=======
-"""test for Doubly linked list."""
-
-
-import pytest
-
->>>>>>> master
 
 
 def test_node_object_exists():
     """Test that the Node Class is working."""
     from dll import Node
-    node = Node(0, 1, 3)
+    node = Node(3)
     assert node
 
-<<<<<<< HEAD
 
 def test_search_found(dll_empty):
     """Test that search will find the given node value in the DLL."""
@@ -48,7 +38,7 @@ def test_size_after_tail_has_been_removed(dll_three_nodes):
     """Test the size of the dll after a tail has been removed."""
     dll_three_nodes.remove(5)
     assert dll_three_nodes.size() == 2
-=======
+
 
 def test_node_has_attributes():
     """Test that Node has attributes."""
@@ -90,29 +80,29 @@ def test_dll_can_take_iterable():
 
 def test_dll_push_adds_new_item():
     """Test that push method add a new item to Linked list."""
-    from dll import push
-    l = dll()
+    from dll import DoublyLinkedList
+    l = DoublyLinkedList()
     l.push('val')
     l.push('val2')
     assert l.head.data == 'val2'
 
-    
+
 def test_push_to_empty_adds_value_to_head(dll_empty):
     """Test that the pushed node gives value to head."""
     dll_empty.push(1)
     assert dll_empty.head.data == 1
 
-    
+
 def test_push_to_empty_sets_head_and_tail(dll_empty):
     """Test to push empty."""
     dll_empty.push(1)
     assert dll_empty.head is dll_empty.tail
 
-    
+
 def test_dll_push_two_new_item():
     """Test that push method adds 2 new items to linked list."""
-    from dll import push
-    l = dll()
+    from dll import DoublyLinkedList
+    l = DoublyLinkedList()
     l.push('val')
     l.push('val2')
     assert l.head.data == 'val2'
@@ -120,26 +110,17 @@ def test_dll_push_two_new_item():
 
 def test_dll_push_moves_old_head_to_new_heads_next():
     """Test that push method properly reassigns head to head node."""
-    from dll import push
-    l = dll()
+    from dll import DoublyLinkedList
+    l = DoublyLinkedList()
     l.push('val')
     l.push('val2')
     assert l.head.next.data == 'val'
 
 
-def test_append():
-    """Test that push method properly reassigns head to head node."""
-    from dll import append
-    l = dll()
-
->>>>>>> master
-
-
 def test_append_adds_value_to_tail(dll_empty):
     """Test that appended value is the tail."""
     dll_empty.append(1)
-    assert dll_empty.tail == tail.data
-    
+    assert dll_empty.tail == dll_empty.tail.data
 
 
 def test_append_multiple_vals_to_tail(dll_empty):
@@ -149,59 +130,24 @@ def test_append_multiple_vals_to_tail(dll_empty):
     dll_empty.append(9)
     assert dll_empty.tail.data == 9
 
-<<<<<<< HEAD
 
-def test_push_to_empty_adds_value_to_head(dll_empty):
-    """Test that the pushed node gives value to head."""
-    dll_empty.push(1)
-    assert dll_empty.head.data == 1
-
-
-def test_push_to_empty_sets_head_and_tail(dll_empty):
-    """Test to push empty."""
-    dll_empty.push(1)
-    assert dll_empty.head is dll_empty.tail
-=======
-
-def test_search_found(dll_empty):
-    """Test that search will find the given node value in the DLL"""
-    dll_empty.push(20)
-    assert dll_empty.search(20)
-
-    
-def test_size_after_tail_has_been_removed(dll_three_nodes):
-    """Test the size of the dll after a tail has been removed"""
-    dll_three_nodes.remove(5)
-    assert dll_three_nodes.size() == 2    
-    
-    
 def test_dll_pop_removes_head_and_returns_value():
     """Test that pop method removes the head and returns its value."""
-    from dll import pop
-    l = dll()
+    from dll import DoublyLinkedList
+    l = DoublyLinkedList()
     l.push('potato')
     l.pop()
     assert l.head is None
-
->>>>>>> master
 
 
 def test_display_empty_dll(dll_empty):
     """Test will display empty."""
     assert dll_empty.display() == "()"
 
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> master
 def test_display_filled_dll(dll_empty):
     """Test to show the the values of all nodes."""
     dll_empty.append(5)
     dll_empty.append(10)
     dll_empty.append(15)
     assert dll_three_nodes.display() == "(5, 10, 15)"
-<<<<<<< HEAD
-=======
-    
->>>>>>> master
