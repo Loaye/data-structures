f48ec5ee03621b4d5fa8f742b95d2225570811f0
Loaye <jr.carstens00@gmail.com>
2017-11-08 14:41:38 -0800

    adds heap to src file

--- src/heap.py
+++ src/heap.py
@@ -1,32 +1,30 @@
 """Create a Max Heap."""
 
-
-# class Node(object):
-#     """Create a node object."""
-
-#     def __init__(self, data, node_left, node_right, parent):
-#         """Initialize a new node."""
-#         self.data = data
-#         self.node_left = node_left
-#         self.node_right = node_right
-#         self.parent = parent
-
-
 class BinaryHeap(object):
     """Instantiates a heap organized by max data values."""
 
-    def __init__(self):
+    def __init__(self, iterable=[]):
         """."""
-        # for i in items:
-        self.heap = []
-        self.size = 0
-        # self.heap.append(i)
-        # self.__floatUp(len(self.heap) - 1)
+        self.heap = [0]
+        self._counter = 0
+        for i in iterable:
+            self.heap.append(i)
+            self._floatup(len(self.heap) - 1)
+
+    def __len__(self):
+        return len(self.heap)
 
     def push(self, data):
         """Push a node into a heap using breadth first."""
-        self.heap.data(data)
+        self.heap.append(data)
         self._floatup(len(self.heap) - 1)
+
+    def peek(self):
+        """See the value in the top node, if there is one."""
+        if self.heap[1]:
+            return self.heap[1]
+        else:
+            return "The list is empty, nothing the see here, move along."
 
     def pop(self):
         """Pop the head, or the largest value, off the heap."""
@@ -40,25 +38,18 @@ class BinaryHeap(object):
             max = False
         return max
 
-    def peek(self):
-        """See the value in the top node, if there is one."""
-        if self.heap[1]:
-            return self.heap[1]
-        else:
-            return "The list is empty, nothing the see here, move along."
-
     def _swap(self, i, j):
         """Swap 2 nodes in the heap for sorting purposes."""
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
-    def _float_up(self, index):
+    def _floatup(self, index):
         """Float the child above the parent if the value is greater."""
         parent = index // 2
         if index <= 1:
             return
         elif self.heap[index] > self.heap[parent]:
             self._swap(index, parent)
-            self.__floatUp(parent)
+            self._floatup(parent)
 
     def _bubbledown(self, index):
         left = index * 2
@@ -71,3 +62,4 @@ class BinaryHeap(object):
         if largest != index:
             self._swap(index, largest)
             self._bubbledown(largest)
+
